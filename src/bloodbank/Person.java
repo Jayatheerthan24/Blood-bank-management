e8300c0754840e8c53aa167984ebf06e029693e9
Jayatheerthan24 <jayatheerthan.p2024cse@sece.ac.in>
2025-09-20 18:12:27 +0530

    person code

--- src/bloodbank/Person.java
+++ src/bloodbank/Person.java
@@ -3,14 +3,12 @@ public class Person {
     private String name;
     private String gender;
     private String contact;
-
     // Constructor
     public Person(String name, String gender, String contact) {
         this.name = name;
         this.gender = gender;
         this.contact = contact;
     }
-
     // Getters and setters
     public String getName() {
         return name;
@@ -18,21 +16,18 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
-
     public String getGender() {
         return gender;
     }
     public void setGender(String gender) {
         this.gender = gender;
     }
-
     public String getContact() {
         return contact;
     }
     public void setContact(String contact) {
         this.contact = contact;
     }
-
     @Override
     public String toString() {
         return "Name: " + name + ", Gender: " + gender + ", Contact: " + contact;
